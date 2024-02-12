7f044538ab68709db56fad34a64e77dfc887cdbd
Sapana <sapana@systha.com>
2024-02-12 20:02:54 +0545

    mass assignment

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        
     ];
+
+    // protected $guarded =[];
 
     /**
      * The attributes that should be hidden for serialization.
